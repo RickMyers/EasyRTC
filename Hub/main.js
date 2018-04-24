5e67b380b88d433cbad54f2c52d6dc720e151dd1
Rick Myers <rickmyers1969@yahoo.com>
2018-04-24 18:17:21 -0400

    De-stinkying

--- Hub/main.js
+++ Hub/main.js
@@ -1,12 +1,14 @@
-/*        _/_/                                            _/    _/  _/    _/  _/_/_/
-       _/    _/  _/  _/_/    _/_/_/  _/    _/    _/_/_/  _/    _/  _/    _/  _/    _/
-      _/_/_/_/  _/_/      _/    _/  _/    _/  _/_/      _/_/_/_/  _/    _/  _/_/_/
-     _/    _/  _/        _/    _/  _/    _/      _/_/  _/    _/  _/    _/  _/    _/
-    _/    _/  _/          _/_/_/    _/_/_/  _/_/_/    _/    _/    _/_/    _/_/_/
-                             _/
-                        _/_/
+/*
 
-    All roads may lead to Rome, but all events go through here....
+ _|_|_|_|                                _|_|_|    _|_|_|_|_|    _|_|_|      _|    _|            _|
+ _|          _|_|_|    _|_|_|  _|    _|  _|    _|      _|      _|            _|    _|  _|    _|  _|_|_|
+ _|_|_|    _|    _|  _|_|      _|    _|  _|_|_|        _|      _|            _|_|_|_|  _|    _|  _|    _|
+ _|        _|    _|      _|_|  _|    _|  _|    _|      _|      _|            _|    _|  _|    _|  _|    _|
+ _|_|_|_|    _|_|_|  _|_|_|      _|_|_|  _|    _|      _|        _|_|_|      _|    _|    _|_|_|  _|_|_|
+                                     _|
+                                 _|_|
+
+    All roads may lead to Rome, but all WebRTC events go through here....
  */
 'use strict';
 String.prototype.pad = function (len,char,left) {
@@ -39,11 +41,6 @@ let io      = require('socket.io').listen(https);
 let users       = { };
 let sockets     = { };
 let observers   = { };
-let dentist     = false;
-let flags       = {
-    "patientWaiting": false,
-    "patient": false
-};
 
 app.get('/', function (req,res) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -52,7 +49,7 @@ app.get('/', function (req,res) {
 
 });
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://dev.argusdentalvision.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -66,8 +63,18 @@ function routeMessage(message,data) {
         }
     }
 }
+function removeListeners(socket_id,listener) {
+    if (listener && observers[listener][socket_id]) {
+        delete observers[listener][socket_id];
+    } else {
+        for (var listener in observers) {
+            if (observers[listener][socket_id]) {
+                delete observers[listener][socket_id];
+            }
+        }
+    }
+}
 io.on('connection', function (socket) {
-    console.log(socket.id+' connected');
     io.to(this.id).emit('registerUserId');
 
     //=========================================================================
@@ -75,11 +82,6 @@ io.on('connection', function (socket) {
     //=========================================================================
     socket.on('disconnect', function () {
         console.log(sockets[this.id]+' disconnected');
-        if (this.id === flags.patient ) {
-            flags.patient = false;
-            flags.patientWaiting = false;
-            io.emit('patientLeft');
-        }
         let user_id = users[this.id];
         //Removing the user from tracking
         if (users[users[this.id]]) {
@@ -89,11 +91,8 @@ io.on('connection', function (socket) {
             delete sockets[this.id];
         }
         //Remove listeners tied to the user
-        for (var listener in observers) {
-            if (observers[listener][this.id]) {
-                delete observers[listener][this.id];
-            }
-        }
+        removeListeners(this.id)
+
         io.emit('userLoggedOut',{ 'user_id': user_id  });
     });
 
@@ -106,14 +105,14 @@ io.on('connection', function (socket) {
         sockets[socket_id] = data.user_id;
         if (users[data.user_id]) {
             console.log('user is already logged in!');
-            users[data.user_id][socket_id] = data.user_id; 
+            users[data.user_id][socket_id] = data.user_id;
         } else {
             users[data.user_id] = { };
             users[data.user_id][socket_id] = data.user_id;
         }
         io.emit('userLoggedIn',{ "user_id": data.user_id } );
     });
-    
+
 
     socket.on('showUsers',function () {
         let ctr = 0;
@@ -127,11 +126,11 @@ io.on('connection', function (socket) {
         }
         console.log("Currently "+ctr+" users are logged in");
     });
-    
+
     socket.on('showListeners', function () {
-        
+
     });
-    
+
     /*
      * Instead of doing 'broadcast' messages, we are going to let clients register
      * which messages they are interested in listening for, and then
@@ -146,8 +145,8 @@ io.on('connection', function (socket) {
         }
         console.log(observers);
     });
-    
-   // socket.on('removeListeners',removeListeners);
+
+    socket.on('removeListeners',removeListeners);
 
     //specifically send to a particular user
     socket.on('RTCUserMessage',function (data) {
@@ -155,7 +154,7 @@ io.on('connection', function (socket) {
             io.to(this.id).emit(data.message,data);
         }
     });
-    
+
     //broadcast to someone listening for a specific event
     socket.on('RTCMessageRelay', function (data) {
         console.log('RTC message: '+data.message);
@@ -168,66 +167,4 @@ io.on('connection', function (socket) {
     socket.on('chatMessage', function (msg) {
         io.emit('chat message',msg);
     });
-
-
-    //=========================================================================
-    //Teledentistry events
-    //=========================================================================
-    socket.on('patientEnteredWaitingRoom', function () {
-        console.log('Someone just entered the waiting room');
-        flags.patientWaiting = true;
-        flags.patient        = this.id;
-        io.emit('patientArrived');
-    });
-
-    socket.on('dentistLoggedOn', function () {
-        console.log('The Dentist Is In');
-        io.emit('dentistIsAvailable');
-    });
-
-    socket.on('patientLeftWaitingRoom', function () {
-        console.log('Someone just left the waiting room');
-        flags.patientWaiting = false;
-        flags.patient = false;
-        io.emit('patientLeft');
-    });
-
-    socket.on('patientWaitingCheck',function () {
-        console.log('checking to see if a patient is waiting');
-        if (flags.patientWaiting) {
-            console.log('A patient is waiting');
-            io.emit('patientWaiting');
-        }
-    });
-
-    socket.on('dentistInitiatedCall', function (data) {
-        console.log('Dentist Initiated Call');
-        io.emit('dentistStartedFacetime',data);
-    });
-
-    socket.on('dentistTerminatedCall', function () {
-        console.log('Dentist Hungup');
-        io.emit('dentistHungup');
-    });
-
-    socket.on('newDentalSnapshot', function () {
-        console.log('A dental wand image was taken');
-        io.emit('newDentalSnapshot');
-    });
-
-    socket.on('teledentistryWandActivated', function () {
-        console.log("The teledentistry wand is active");
-        io.emit('teledentistryWandInUse');
-    });
-
-    socket.on('teledentistryFormChange',function (data) {
-        console.log('The dental form has changed - '+data.form_id);
-        io.emit('teledentistryFormUpdate',data);
-    });
-
-    socket.on('teledentistryToothPicked',function (data) {
-        console.log('A tooth has been highlighted or deselected');
-        io.emit('teledentistryToothChosen',data);
-    });
-
 });
